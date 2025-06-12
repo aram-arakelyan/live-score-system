5a06f7b776875669e0c31cbae0900ad1e9c1eda8
aram-arakelyan <ar.arakelyan@gmail.com>
2025-06-12 18:11:24 +0400

    Initialize multi-module project structure

---
+++ src/test/java/com/example/livescoresystem/LiveScoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livescoresystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveScoreSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
